f0a8b7d1d9529f5f15d855d1e2eb707cea420887
StanislaVailevich <stanislavasilevich@yandex.ru>
2025-01-22 18:05:53 +0300

    add text index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-'Hello, Hexlet!'
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается напрямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
+
+echo 'hello';

7a49a1da025ba75d21361170ebe061b2c8fa003d
StanislaVailevich <stanislavasilevich@yandex.ru>
2025-01-25 20:25:28 +0300

    remove .idea from repo

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-// Файл не включается напрямую
-// Он загрузится автоматически благодаря автозагрузке
-use Hexlet\Php\Runner;
-
-print_r(Runner\run());
-
-echo 'hello';
+<html>
+<head>
+  <title>PHP Test</title>
+</head>
+<body>
+<?php echo '<p>Hello World</p>'; ?>
+</body>
+</html>
